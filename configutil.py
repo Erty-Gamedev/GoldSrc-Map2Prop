d6f585753e6237369d357331eed06eeb8d1668dc
Erty <erty.gamedev@gmail.com>
2023-07-12 12:54:50 +0200

    Added CLI arguments for various .qc commands

--- configutil.py
+++ configutil.py
@@ -24,39 +24,90 @@ class ConfigUtil:
             with filepath.open('w') as configfile:
                 self.config.write(configfile)
 
-    def parseargs(self, running_as_exe: bool):
-        parser = argparse.ArgumentParser(
+        self.parser = argparse.ArgumentParser(
             prog='GoldSrc Map2Prop',
             description='Converts a .rmf/.jmf or J.A.C.K .obj into goldsrc\
 .smd files for model creation.',
             exit_on_error=False
         )
 
+    def app_exit(self, status: int = 0, message: str = ''):
+        self.parser.exit(status, message)
+
+    def parseargs(self, running_as_exe: bool):
         if True or running_as_exe:
-            parser.add_argument('input', nargs='?', type=str,
-                                help='.rmf/.jmf/.obj file to convert')
+            self.parser.add_argument('input', nargs='?', type=str,
+                                     help='.rmf/.jmf/.obj file to convert')
 
-        parser.add_argument('-o', '--output', type=str,
-                            help='specify an output directory')
-        parser.add_argument('-g', '--game_config', type=str,
-                            help='game setup to use from config.ini')
-        parser.add_argument('-m', '--studiomdl', type=str,
-                            help='path to SC studiomdl.exe')
-        parser.add_argument('-w', '--wad_list', type=str,
-                            help='path to text file listing .wad files')
-        parser.add_argument('-c', '--wad_cache', type=int,
-                            help='max number of .wad files to keep in memory',)
-        parser.add_argument('-a', '--autocompile', action='store_true',
-                            help='compile model after conversion')
-        parser.add_argument('-s', '--smoothing', action='store_true',
-                            help='apply smooth shading to model')
-        parser.add_argument('-t', '--smoothing_threshold', type=float,
-                            help='angle threshold for applying smoothing')
+        # App / model general arguments
+        self.parser.add_argument(
+            '-o', '--output', type=str,
+            help='specify an output directory')
+        self.parser.add_argument(
+            '-g', '--game_config', type=str,
+            help='game setup to use from config.ini')
+        self.parser.add_argument(
+            '-m', '--studiomdl', type=str,
+            help='path to SC studiomdl.exe')
+        self.parser.add_argument(
+            '-w', '--wad_list', type=str,
+            help='path to text file listing .wad files')
+        self.parser.add_argument(
+            '-c', '--wad_cache', type=int,
+            help='max number of .wad files to keep in memory',)
+        self.parser.add_argument(
+            '-a', '--autocompile', action='store_true',
+            help='compile model after conversion')
+        self.parser.add_argument(
+            '-s', '--smoothing', action='store_true',
+            help='apply smooth shading to model')
+        self.parser.add_argument(
+            '-t', '--smoothing_threshold', type=float,
+            help='angle threshold for applying smoothing')
 
-        self.args = parser.parse_args()
+        # .qc specific arguments
+        self.parser.add_argument(
+            '--outputname', type=str,
+            help='filename for the finished model')
+        self.parser.add_argument(
+            '--scale', type=float,
+            help='scale the model by this amount')
+        self.parser.add_argument(
+            '--gamma', type=float,
+            help='darken/brighten textures (default 1.8)')
+        self.parser.add_argument(
+            '--offset', nargs=3,
+            help='X Y Z offset to apply to the model')
+        self.parser.add_argument(
+            '--rotate', type=float,
+            help='rotate the model by this many degrees')
+
+        self.args = self.parser.parse_args()
         if self.args.input is None:
             raise IndexError()
         self.input = self.args.input
+
+    @property
+    def qc_outname(self) -> str:
+        return self.args.outputname if self.args.outputname else None
+
+    @property
+    def qc_scale(self) -> float:
+        return self.args.scale if self.args.scale else 1.0
+
+    @property
+    def qc_gamma(self) -> float:
+        return self.args.gamma if self.args.gamma else 1.8
+
+    @property
+    def qc_offset(self) -> str:
+        if self.args.offset:
+            return ' '.join([f"{i}" for i in self.args.offset])
+        return '0 0 0'
+
+    @property
+    def qc_rotate(self) -> float:
+        return (270.0 + (self.args.rotate if self.args.rotate else 0.0)) % 360
 
     @property
     def output_dir(self) -> Path:
