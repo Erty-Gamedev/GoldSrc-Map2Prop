4aff800cbc2ae301532cf806779d6a5b9093bdde
Erty <erty.gamedev@gmail.com>
2024-02-16 15:57:39 +0100

    Parse polygonal data from planes

--- formats/map_reader.py
+++ formats/map_reader.py
@@ -7,9 +7,17 @@ Created on Fri Jul 21 15:31:22 2023
 
 from PIL import Image
 from pathlib import Path
-from geoutil import PolyFace, triangulate_face, Vector3D, plane_intersection
-from formats import Face
+from geoutil import (PolyFace, PolyPoint, Plane, triangulate_face, Vector3D,
+                     intersection_3planes, sort_vertices)
+from formats import MissingTextureException
 from formats.wad_handler import WadHandler
+
+
+def is_vertex_outside(vertex, planes: list) -> bool:
+    for plane in planes:
+        if plane.point_relation(vertex) > 0:
+            return True
+    return False
 
 
 class Entity:
@@ -24,10 +32,28 @@ class Brush:
         self.faces = faces
 
 
-class Plane:
-    def __init__(self, plane_points: list, texture: dict):
-        self.plane_points = plane_points
+class Face:
+    def __init__(self, vertices: list, texture: dict, normal: Vector3D):
+        self.vertices = sort_vertices(vertices, normal)
         self.texture = texture
+
+        self.polypoints = []
+
+        nu, nv = texture['rightaxis'], texture['downaxis']
+        w, h = texture['width'], texture['height']
+        su, sv = texture['scalex'], texture['scaley']
+        ou, ov = texture['shiftx'], texture['shifty']
+
+        for vertex in self.vertices:
+            u = (vertex.dot(nu)/w)/su + ou/w
+            v = (vertex.dot(nv)/h)/sv + ov/h
+
+            self.polypoints.append(PolyPoint(
+                vertex,
+                Vector3D(u, -v, 0),
+                normal
+            ))
+        # TODO: normalize UV
 
 
 class MapReader:
@@ -88,32 +114,40 @@ class MapReader:
             elif line.startswith('{'):
                 brush = self.readbrush(file)
                 brushes.append(brush)
+                self.brushes.append(brush)
             elif line.startswith('}'):
                 break
             else:
                 raise Exception(f"Unexpected entity data: {line}")
         return Entity(classname, properties, brushes)
 
-
     def readbrush(self, file) -> Brush:
-        faces = []
-        protofaces = []
         planes = []
+
         while line := file.readline().strip():
             if line.startswith('//'):
                 continue
             elif line.startswith('('):
-                protoface = self.readface(line)
-                protofaces.append(protoface)
-                planes.extend(protoface.plane_points)
+                planes.append(self.readplane(line))
             elif line.startswith('}'):
                 break
             else:
                 raise Exception(f"Unexpected face data: {line}")
-        for protoface in protofaces:
-            vertices = self.intersections(protoface.plane_points, planes)
-            faces.append(Face(
-                vertices, protoface.plane_points, protoface.texture))
+
+        faces = self.faces_from_planes(planes)
+
+        for face in faces:
+            if self.wadhandler.skip_face(face):
+                continue
+
+            self.addpolyface(face)
+            for polypoint in face.polypoints:
+                if polypoint not in self.allpolypoints:
+                    self.allpolypoints.append(polypoint)
+                    if polypoint.v not in self.vn_map:
+                        self.vn_map[polypoint.v] = []
+                    self.vn_map[polypoint.v].append(polypoint.n)
+
         return Brush(faces)
 
     def readplane(self, line: str) -> Plane:
@@ -129,10 +163,10 @@ class MapReader:
 
         texture = {
             'name': parts[15],
-            'rightaxis': (
+            'rightaxis': Vector3D(
                 float(parts[17]), float(parts[18]), float(parts[19])),
             'shiftx': float(parts[20]),
-            'downaxis': (
+            'downaxis': Vector3D(
                 float(parts[23]), float(parts[24]), float(parts[25])),
             'shifty': float(parts[26]),
             'angle': float(parts[28]),
@@ -142,43 +176,90 @@ class MapReader:
             'height': 16,
         }
 
+        if texture['name'].lower() not in self.wadhandler.SKIP_TEXTURES:
+            tex_image = self.get_texture(texture['name'])
+            texture['width'] = tex_image['width']
+            texture['height'] = tex_image['height']
+        else:
+            texture['width'] = 16
+            texture['height'] = 16
+
+        # Check if texture exists, or try to extract it if not
+        if texture['name'] not in self.checked:
+            if not self.wadhandler.check_texture(texture['name']):
+                self.missing_textures = True
+
+            # Make note of masked textures
+            if (texture['name'].startswith('{')
+                    and texture['name'] not in self.maskedtextures):
+                self.maskedtextures.append(texture['name'])
+            self.checked.append(texture['name'])
+
         return Plane(plane_points, texture)
 
-    def faces_from_planes(self, planes: list) -> dict:
-        vertices = []
-        faces = {}
-        edges = []
-        edge_face_map = {}
+    def faces_from_planes(self, planes: list) -> list:
+        num_planes = len(planes)
+        faces = [{'vertices': []} for n in range(num_planes)]
 
-        for plane in planes:
-            for plane2 in planes:
-                if plane == plane2:
-                    continue
-                if (edge := plane_intersection(plane, plane2)) is not None:
-                    edges.append(edge)
-                    if edge not in edge_face_map:
-                        edge_face_map[edge] = []
-                    if plane not in edge_face_map[edge]:
-                        edge_face_map[edge].append(plane)
-                    if plane2 not in edge_face_map[edge]:
-                        edge_face_map[edge].append(plane2)
+        for i in range(num_planes):
+            for j in range(i + 1, num_planes):
+                for k in range(j + 1, num_planes):
+                    if i == j == k:
+                        continue
 
-        for edge in edges:
-            for edge2 in edges:
-                if edge == edge2:
-                    continue
-                # if (vertex := line_intersection(edge, edge2)) is not None:
-                #     vertices.append(vertex)
+                    vertex = intersection_3planes(
+                        planes[i], planes[j], planes[k]
+                    )
 
-        return faces
+                    if vertex is False:
+                        continue
 
-    # def intersections(self, plane: list, neighbours: list):
-    #     vertices = []
+                    if is_vertex_outside(vertex, planes):
+                        continue
 
-    #     for n in neighbours:
-    #         if n == plane:
-    #             continue
+                    faces[i]['vertices'].append(vertex)
+                    faces[j]['vertices'].append(vertex)
+                    faces[k]['vertices'].append(vertex)
 
-    #         vertices.append((0, 0, 0))
+                    faces[i]['texture'] = planes[i].texture
+                    faces[j]['texture'] = planes[j].texture
+                    faces[k]['texture'] = planes[k].texture
 
-    #     return vertices
+                    faces[i]['normal'] = planes[i].normal
+                    faces[j]['normal'] = planes[j].normal
+                    faces[k]['normal'] = planes[k].normal
+
+        for f in faces:
+            if len(f['vertices']) < 3:
+                raise Exception('uh oh')
+
+        return [Face(f['vertices'], f['texture'], f['normal']) for f in faces]
+
+    def addpolyface(self, face: Face):
+        tris = triangulate_face(face.vertices)
+
+        for tri in tris:
+            tri_face = []
+            for p in tri:
+                for polyp in face.polypoints:
+                    if p == polyp.v:
+                        tri_face.append(polyp)
+                        break
+
+            polyface = PolyFace(tri_face, face.texture['name'])
+
+            self.allfaces.append(polyface)
+
+    def get_texture(self, texture: str) -> Image:
+        if texture not in self.textures:
+            texfile = self.filedir / f"{texture}.bmp"
+            if not texfile.exists():
+                raise MissingTextureException(
+                    f"Could not find texture {texture}")
+
+            with Image.open(texfile, 'r') as imgfile:
+                self.textures[texture] = {
+                    'width': imgfile.width,
+                    'height': imgfile.height
+                }
+        return self.textures[texture]
